9a8aa3ffa601bffc48168f34578ae9074df39465
Andre Fernando Machado <andrefernando777@hotmail.com>
2025-11-28 23:41:51 -0300

    sobre

---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
+export function About() {
+        const navigate = useNavigate();
+
+    return <div className="p-20 text-center">Sobre Nós (Em breve)</div>
+}
